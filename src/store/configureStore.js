d923c7311562f1b2a2f28d401a312053ef4f6eea
Hennadiy <gena.utyf@gmail.com>
2018-05-11 15:28:26 +0300

    Reduce production bundle size

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,21 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from 'reducers';
 import initialState from './initialState';
 
 const configureStore = () => {
   const middleware = [thunk];
   if (process.env.NODE_ENV !== 'production') {
-    const logger = require('redux-logger').default;
-    middleware.push(logger);
-    const reduxImmutableStateInvariant = require('redux-immutable-state-invariant')
-      .default;
-    middleware.push(reduxImmutableStateInvariant());
+    middleware.push(require('redux-logger').default);
+    middleware.push(require('redux-immutable-state-invariant').default());
   }
 
   let enhancer = applyMiddleware(...middleware);
   if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
     enhancer = composeWithDevTools(enhancer);
   }
   return createStore(rootReducer, initialState, enhancer);
