9b3c8245f37672a1352840191e63792387e27659
Hennadiy <gena.utyf@gmail.com>
2018-05-11 14:46:30 +0300

    Added prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
